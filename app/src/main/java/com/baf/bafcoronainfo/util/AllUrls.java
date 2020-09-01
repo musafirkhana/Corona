4c29838a5fb5e602e63adf7be02f77448bff52ed
Musafir <ali.mbstu.ict@gmail.com>
2020-09-01 08:55:34 +0600

    Acces Token Add

--- app/src/main/java/com/baf/bafcoronainfo/util/AllUrls.java
+++ app/src/main/java/com/baf/bafcoronainfo/util/AllUrls.java
@@ -5,12 +5,12 @@ public class AllUrls {
 
     public static String APP_KEY = "ABCD";
     public static String BASE_URL = "https://skyapi.website/api/";
-    public static String BASE_WISE_KEY = "basewisetotal";
-    public static String TODAY_KEY = "today";
-    public static String YESTREDAY_KEY = "yesterday";
+//    public static String BASE_URL = "https://baf.mil.bd/coronainfo/api/";
+    public static String TOKEN="5FJktjS6WWhYL7Ku2WeUBHhVuUFxLmnfHypMurvpUfEWe8T3fVzXE9ZFg7wpzrktMhkQRpFGAh5huQUXJ2HLGmHnEZYvft9jAYvNcTkE2eMfdkap3kR9jJcucbj98CKn";
+    public static String BASE_WISE_KEY = "basewisetotal?api_token="+TOKEN;
+    public static String TODAY_KEY = "today?api_token="+TOKEN;
+    public static String YESTREDAY_KEY = "yesterday?api_token="+TOKEN;
 
     public static String API_KEY = "";
-    //public static String HTTP = "http://192.168.1.4:80/";
-    //sudo php artisan serve --host 192.168.1.4 --port 80
     public static String COUNTRY_API="https://coronavirus-19-api.herokuapp.com/countries/bangladesh";
 }
