5a3ac4ae5601456ffb32c0d4c033381d7f2ca2fd
Musafir <ali.mbstu.ict@gmail.com>
2020-09-01 14:22:23 +0600

    Acces Token Add

--- app/src/main/java/com/baf/bafcoronainfo/util/AllUrls.java
+++ app/src/main/java/com/baf/bafcoronainfo/util/AllUrls.java
@@ -4,8 +4,8 @@ package com.baf.bafcoronainfo.util;
 public class AllUrls {
 
     public static String APP_KEY = "ABCD";
-    public static String BASE_URL = "https://skyapi.website/api/";
-//    public static String BASE_URL = "https://baf.mil.bd/coronainfo/api/";
+//    public static String BASE_URL = "https://skyapi.website/api/";
+    public static String BASE_URL = "https://www.baf.mil.bd/coronainfo/api/";
     public static String TOKEN="5FJktjS6WWhYL7Ku2WeUBHhVuUFxLmnfHypMurvpUfEWe8T3fVzXE9ZFg7wpzrktMhkQRpFGAh5huQUXJ2HLGmHnEZYvft9jAYvNcTkE2eMfdkap3kR9jJcucbj98CKn";
     public static String BASE_WISE_KEY = "basewisetotal?api_token="+TOKEN;
     public static String TODAY_KEY = "today?api_token="+TOKEN;
